bb3e91ea7a71d4e6b07b2bebcff85ebec6c17a84
azizca7 <ikromovazizbek256@gmail.com>
2024-11-28 20:35:16 +0500

    end

---
+++ main.js
@@ -0,0 +1,639 @@
+// let month = +prompt("oy nig raqamini kiriting");
+
+// switch (month) {
+//   case 1:
+//     console.log("Yanvar oyida 31 kun bor");
+//     break;
+//   case 2:
+//     console.log("Fevral oyida 28 kun bor");
+//     break;
+//   case 3:
+//     console.log("Mart oyida 31 kun bor");
+//     break;
+//   case 4:
+//     console.log("Aprel oyida 30 kun bor");
+//     break;
+//   case 5:
+//     console.log("May oyida 31 kun bor");
+//     break;
+//   case 6:
+//     console.log("Iyun oyida 30 kun bor");
+//     break;
+//   case 7:
+//     console.log("Iyul oyida 31 kun bor");
+//     break;
+//   case 8:
+//     console.log("Avgust oyida 31 kun bor");
+//     break;
+//   case 9:
+//     console.log("Sentyabr oyida 30 kun bor");
+//     break;
+//   case 10:
+//     console.log("Oktyabr oyida 31 kun bor");
+//     break;
+//   case 11:
+//     console.log("Noyabr oyida 30 kun bor");
+//     break;
+//   case 12:
+//     console.log("Dekabr oyida 31 kun bor");
+//     break;
+
+//   default:
+//     console.log("Bunday oy mavjud emas!");
+//     break;
+// }
+
+// 1-misol
+
+// for (let i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// 2-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 3-misol
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// 4-misol
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i ** 2);
+// }
+
+// 5-misol
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// 6-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+
+// console.log(sum);
+
+// 7-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 8-misol
+
+// let multiply = 1;
+
+// // for (let i = 1; i <= 15; i++) {
+// //   if (i % 2 == 1) {
+// //     sum = sum * i;
+// //   }
+// // }
+// // console.log(sum);
+
+// 9-misol
+
+// for (let i = 5; i <= 25; i++) {
+// console.log(i ** 3);
+
+// }
+
+// 10-misol
+
+// for (let num = 1; num <= 10; num++) {
+
+//   let sqrt = 0;
+
+//   for (let i = 1; i * i <= num; i++) {
+//     sqrt = i;
+//   }
+
+//   console.log(sqrt);
+// }
+
+// 11-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 12-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum = sum + i;
+// }
+// console.log(sum);
+
+// 13-misol
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 14-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   sum = sum + i;
+// }
+// console.log(sum / 100);
+
+// 15-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 16-misol
+
+// let n = +prompt("n kiriting");
+
+// for (let i = 1; i < n; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 17-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 15 == 0 ) {
+//     console.log(i);
+//   }
+// }
+
+// 18-misol
+
+// sum = 0;
+
+// for (let i = 1; i <= 30; i++) {
+//   sum += i ** 2;
+// }
+// console.log(sum);
+
+// 19-misol
+
+// let count = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// 20-misol
+
+// let n = +prompt("n ni kiriting:");
+// let n2 = +prompt("n2 ni kiriting:");
+
+// let sum = 0;
+
+// for (let i = n; i <= n2; i++) {
+//   sum += i;
+// }
+
+// console.log(sum);
+
+// 21-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// 22-misol
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log("Jusft son");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// 23-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 10 != 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 24-misol
+
+// multiply = 1;
+
+// for (let i = 1; i <= 30; i++) {
+//   multiply = multiply * i;
+// }
+
+// console.log(multiply);
+
+// 25-misol
+
+// let n1 = +prompt(`son kiritng:`);
+// let n2 = +prompt(`son kiritng:`);
+
+// for (let i = n1; i <= n2; i++) {
+//   console.log(i);
+// }
+
+// 27-misol
+
+// let num = +prompt("son kiriting:");
+
+// let factorial = 1;
+
+// for (let i = 1; i <= num; i++) {
+//   factorial = factorial * i;
+// }
+
+// console.log(factorial);
+
+// 28-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 29-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % i == 0 && i % 1 == 0) {
+//     console.log(i);
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// 30-misol
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(`${i} - Bu sonning kvadrati: ${i * i}`);
+// }
+
+// 31-misol
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 == 0) {
+//     console.log(i * i);
+//   }
+// }
+
+// 32-misol
+
+// for (let i = 0; i <=30; i += 10) {
+//   console.log(i * 10);
+
+// }
+
+// 34-misol
+
+// let n = +prompt(`son kiritng:`);
+
+// if (n > 0) {
+// }
+
+// let sum = 0;
+
+// for (let i = 1; i < n; i++) {
+//   if (i % 2 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// 36-misol
+
+// let n = +prompt(`son kiritng:`);
+
+// for (let i = 1; i <= n; i++) {
+//   if (i % 10 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 37-misol
+
+// sum = 0;
+
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i ** 2);
+//   sum += i ** 2;
+// }
+
+// console.log(sum);
+
+//
+
+// 38-misol
+
+// let factorial = 1;
+
+// for (let i = 1; i <= 10; i++) {
+//   factorial = factorial * i;
+// }
+
+// console.log(factorial);
+
+// 39-misol
+
+// let n = +prompt("n ni kiritng:")
+
+// sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i ** 2);
+//   sum += i ** 2;
+// }
+
+// console.log(sum);
+
+// 40-misol
+
+// let n = +prompt(`son kiritng:`);
+
+// if (n > 0) {
+// }
+
+// for (let i = n - 1; i > 0; i--) {
+//   console.log(i);
+// }
+
+// 41-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i ** 3);
+
+//   }
+// }
+
+// 42-misol
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 != 0) {
+//     console.log(i);
+//   }
+// }
+
+// 43-misol
+
+// let n = +prompt(`son kiritng:`);
+
+// if (n > 0) {
+// }
+
+// sum = 0;
+
+// for (let i = n - 1; i > 0; i--) {
+//   console.log(i);
+//   if (i % 3 == 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 44-misol
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`${i} - Juft son`);
+//   } else {
+//     console.log(`${i} - Toq son`);
+//   }
+// }
+
+// 45-misol
+
+// sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i ** 2);
+
+//     sum += i ** 2;
+//   }
+// }
+
+// console.log(sum);
+
+// 46-misol
+
+// let n = +prompt("n ni kiriting:");
+
+// for (let i = n; i * i <= 100; i++) {
+//   console.log(i * i);
+// }
+
+// 47-misol
+
+// sum = 0;
+
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 3 == 0 && i % 4 == 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 48-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// 49-misol
+
+// let n = +prompt("n ni kiriting:");
+
+// multiply = 1;
+
+// for (let i = n - 1; i > 0; i--) {
+//   if (i % 7 == 0) {
+//     multiply *= i;
+//   }
+// }
+
+// console.log(multiply);
+
+// 50-misol
+
+// let n1 = +prompt("n1 ni kiriting:");
+// let n2 = +prompt("n2 ni kiriting:");
+
+// sum = 0;
+
+// for (let i = n1; i < n2; i++) {
+//   console.log(i ** 2);
+//   sum += i ** 2;
+// }
+
+// console.log(sum);
+
+//  Qo'shimcha misollar
+
+// 1-misol
+
+// let n = +prompt("n ni kiriting:");
+// if (n > 0) {
+// }
+
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     console.log(i);
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 2-misol
+
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % i == 0 && i % 1 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// 3-misol
+
+// let n1 = +prompt("n1 ni kiriting:");
+// let n2 = +prompt("n2 ni kiriting:");
+
+// if (n1 && n2 > 0) {
+// }
+
+// for (let i = n1; i < n2; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i ** 2);
+//   }
+// }
+
+// 4-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   console.log(i);
+
+//   if (i % 5 == 0) {
+//     console.log("*");
+//   }
+// }
+
+// 5-misol
+
+// let n = +prompt("son kiriting:");
+
+// for (let i = n - 1; i > 0; i--) {
+//   console.log(i);
+// }
+
+// 6-misol
+
+// multiply = 1;
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 4 == 0 && i % 6 == 0) {
+//     multiply *= i;
+//   }
+// }
+// console.log(multiply);
+
+// 7-misol
+
+// let n = prompt("son kiritng");
+// let factorial = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   factorial = factorial * i;
+// }
+
+// console.log(factorial);
+
+// 9-misol
+
+// let n1 = +prompt("son kiriting:");
+// let n2 = +prompt("son kiriting:");
+
+// count = 1;
+
+// for (let i = n1; i <= n2; i++) {
+//   if (i % 2 == 1) {
+//     console.log(`${count} - ${i}`);
+//     count++;
+//   }
+// }
+
+// 16-misol
+
+// let n = prompt("son kiriting:");
+
+// reverse = "";
+
+// for (let i = n.length - 1; i >= 0; i--) {
+//   reverse += n[i]
+// }
+
+// console.log(reverse);
+
+// 17-misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 == 0) {
+//     console.log("Juft");
+//   } else {
+//     console.log("Toq");
+//   }
+// }
